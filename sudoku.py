ec9112e49608ca599e434176a421bfd024f09afc
Konstantinos Sotiropoulos <kostis.sotiropoulos@protonmail.com>
2026-06-21 12:32:59 +0200

    Parse arguments, produce outputs, and "fail" test cases

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+
+import numpy as np
+import yaml
+
 
 class Sudoku(object):
-    def __init__(self, grid):
+    def __init__(self, grid: np.ndarray):
         self.grid = grid
 
     def __str__(self):
@@ -10,11 +15,9 @@ class Sudoku(object):
     def solve(self):
         pass
 
-def run_sudoku(input):
-    sudoku = Sudoku(input)
+def run_sudoku(grid: np.ndarray) -> np.ndarray:
+    sudoku = Sudoku(grid)
     sudoku.solve()
-    print(sudoku)
-    
     return sudoku.grid
 
 def compare(source, reference):
@@ -24,4 +27,20 @@ def compare(source, reference):
                 return False
     
     return True
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Solve Sudokus, store their solution and if a reference is provided, compare against it")
+    parser.add_argument("test_spec", help="A yaml containing a list of dicts for the input/output/reference .npy files")
+    args = parser.parse_args()
+
+    with open(args.test_spec) as f:
+        test_spec = yaml.safe_load(f)
+
+    for tc in test_spec:
+        out = run_sudoku(np.load(tc["input"]))
+
+        if "reference" in tc and not compare(out, np.load(tc["reference"])):
+            print(f"Solution does not match reference for: {tc['input']}")
+
+        np.save(tc["output"], out)
 
