e18776fc0649bba7f6bd8e87f4225e81ec68e702
Konstantinos Sotiropoulos <kostis.sotiropoulos@protonmail.com>
2026-06-22 18:10:35 +0200

    Add a sudoku that can't be solved by our trivial solver

--- unit_tests.py
+++ unit_tests.py
@@ -53,6 +53,45 @@ class TestSudoku(unittest.TestCase):
         with self.assertRaises(ValueError):
             _ = Sudoku(invalid)
 
+    def test_backtracking(self):
+        grid = np.array(
+            [
+                [0, 3, 0, 0, 0, 7, 2, 0, 0],
+                [8, 0, 0, 0, 0, 0, 0, 0, 5],
+                [6, 0, 0, 8, 0, 0, 4, 0, 3],
+                [0, 0, 3, 0, 9, 5, 0, 0, 0],
+                [4, 0, 0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 2, 1, 0, 6, 0, 0],
+                [7, 0, 5, 0, 0, 6, 0, 0, 2],
+                [2, 0, 0, 0, 0, 0, 0, 0, 7],
+                [0, 0, 1, 4, 0, 0, 0, 9, 0]
+            ]
+        )
+
+        s = Sudoku(grid)
+        s.solve()
+        self.assertTrue(s.is_solved())
+
+        solution = np.array(
+            [
+                [9, 3, 4, 1, 5, 7, 2, 6, 8],
+                [8, 1, 2, 6, 4, 3, 9, 7, 5],
+                [6, 5, 7, 8, 2, 9, 4, 1, 3],
+                [1, 6, 3, 7, 9, 5, 8, 2, 4],
+                [4, 2, 9, 3, 6, 8, 7, 5, 1],
+                [5, 7, 8, 2, 1, 4, 6, 3, 9],
+                [7, 4, 5, 9, 3, 6, 1, 8, 2],
+                [2, 9, 6, 5, 8, 1, 3, 4, 7],
+                [3, 8, 1, 4, 7, 2, 5, 9, 6]
+            ]
+        )
+        # sanity check that I transfered the solution correctly
+        solved = Sudoku(solution)
+        self.assertTrue(solved.is_solved())
+
+        # check that the solution is correct
+        self.assertTrue( (s.grid == solution).all() )
+
 
 if __name__ == "__main__":
     unittest.main()
